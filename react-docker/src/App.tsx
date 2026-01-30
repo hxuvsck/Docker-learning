97a0057e0ec7a9252097128ba54bae19213bfa35
hxuvsck <hxuvsck@gmail.com>
2026-01-30 16:55:01 +0800

    Successfully configured vite config by allowing Docker HMR

--- react-docker/src/App.tsx
+++ react-docker/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Docker is awesome! Hi</h1>
+      <h1>Docker is awesome! Bye</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- react-docker/vite.config.ts
+++ react-docker/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // expose to Docker
+    watch: {
+      usePolling: true, // REQUIRED for Docker bind mounts
+      interval: 100,    // optional but recommended
+    },
+  },
 })
